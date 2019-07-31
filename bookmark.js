e8fdeb959945ea86e39d710fc733ddee66fdb2b5
Dieptran99 <52845702+Dieptran99@users.noreply.github.com>
2019-08-01 01:02:56 +0700

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,5 +1,5 @@
 INIT:
-javascript:MA=["112087","688565","112084","690065","112079","689997","112097","688499","112099","688541","112110"]; i=-1;
+javascript:MA=["112111","112107","112053","112089","112084","112079","112097","688499","690090","690042","688565"]; i=-1;
 
 AUTO:
 javascript: i++; CL=document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_btDirectClassRegister_CD"); DC=document.getElementById("ctl00_ctl00_ASPxSplitter1_Content_ContentSplitter_MainContent_ASPxCallbackPanel1_tbDirectClassRegister_I"); DC.value=MA[i]; CL.click();
